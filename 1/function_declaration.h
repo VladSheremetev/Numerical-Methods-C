24b153c25c32e32c11d29b6085ba141c8cdb1be5
Vladislav Sheremetev <68332331+VladSheremetev@users.noreply.github.com>
2021-12-18 10:10:16 +0300

    Update function_declaration.h

--- 1/function_declaration.h
+++ 1/function_declaration.h
@@ -8,14 +8,14 @@
 #include <fstream>
 #include <string>
 
-double*     x_rand_gen(double, double, size_t);
-double*     y_gen(double*, size_t);
-void        print_array(double*, size_t);
+double*   x_rand_gen(double, double, size_t);
+double*   y_gen(double*, size_t);
+void      print_array(double*, size_t);
 double  	lagrange(double*, double*, size_t, double);
 double*  	x_gen(double, size_t, double);
-double*   	l_gen(double*, double*, double*, size_t, size_t);
-void 		print_file(std::string, double*, double*, size_t);
-bool 		search_point(double*, size_t, double);
-void 		quick_sort(double*, size_t, size_t);
+double*   l_gen(double*, double*, double*, size_t, size_t);
+void 		  print_file(std::string, double*, double*, size_t);
+bool 		  search_point(double*, size_t, double);
+void 		  quick_sort(double*, size_t, size_t);
 
 #endif
